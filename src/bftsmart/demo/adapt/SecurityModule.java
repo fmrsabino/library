8a4f1a6dbd052eaf926ce08f43cbe6a5eb4e8d32
fmrsabino <fredx96@gmail.com>
2016-02-21 23:12:28 +0000

    Initial version where the Security Module can add/remove replicas

--- src/bftsmart/demo/adapt/SecurityModule.java
+++ src/bftsmart/demo/adapt/SecurityModule.java
@@ -1,14 +1,20 @@
 package bftsmart.demo.adapt;
 
+import bftsmart.reconfiguration.VMServices;
 import bftsmart.tom.ServiceProxy;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 public class SecurityModule {
 
     private static int processId;
     private static int threatLevel;
+
+    private static final int ADD_REPLICA = 1;
+    private static final int REMOVE_REPLICA = -1;
 
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -22,7 +28,7 @@ public class SecurityModule {
         threatLevel = Integer.parseInt(args[1]);
 
         sendThreatLevel("adapt-config");
-        sendThreatLevel("");
+        //sendThreatLevel("");
     }
 
     private static void sendThreatLevel(String configHome) {
@@ -33,6 +39,25 @@ public class SecurityModule {
 
             System.out.println("Sending threat level of: " + threatLevel);
             byte[] reply = serviceProxy.invokeUnordered(out.toByteArray());
+            if (reply != null) {
+                DataInputStream dis = new DataInputStream(new ByteArrayInputStream(reply));
+                String line = dis.readUTF();
+                dis.close();
+                System.out.println(line);
+                String[] parts = line.toString().split(" ");
+                for (int i = 0; i < parts.length; i++) {
+                    System.out.println(i + " - " + parts[i]);
+                }
+                if (parts.length == 3) {
+                    System.out.println("Sending ADD command");
+                    VMServices.main(new String[] {parts[0], parts[1], parts[2]});
+                } else if (parts.length == 1) {
+                    System.out.println("Sending RM command");
+                    VMServices.main(new String[] {parts[0]});
+                } else {
+                    System.out.println("else");
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
