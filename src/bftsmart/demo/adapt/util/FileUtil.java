66b168c9d5cd662140a21cc8c61622ffd5cb3ad3
fmrsabino <fredx96@gmail.com>
2016-04-11 16:01:39 +0100

    Improved FileUtil

--- src/bftsmart/demo/adapt/util/FileUtil.java
+++ src/bftsmart/demo/adapt/util/FileUtil.java
@@ -4,16 +4,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileUtil {
     public static List<String> readFileLines(String filePath) {
-        List<String> result = new ArrayList<>();
         try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
-            stream.forEach(result::add);
+            return stream.collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
+            return new ArrayList<>();
         }
-        return result;
     }
 }
