ccfc4b4f5095e76a0a55f8f010fa2161d2f174e4
Daniel Porto <daniel.porto@gmail.com>
2016-05-06 18:55:48 +0100

    Making the Bandwidth sensor issue a value per second for 10s

--- src/bftsmart/demo/adapt/sensors/BandwidthSensor.java
+++ src/bftsmart/demo/adapt/sensors/BandwidthSensor.java
@@ -6,7 +6,28 @@ import bftsmart.demo.adapt.util.Constants;
 
 public class BandwidthSensor {
     public static void main(String[] args) {
-        byte[] reply = BftUtils.sendMessage(1003, Constants.ADAPT_HOME_FOLDER, new BandwidthMessage(10), true);
-        System.out.println(reply);
+
+        new Thread(){
+            public void run(){
+                while(true){
+                    System.out.println("BandwidthSensor.run before sending message");
+                    byte[] reply = BftUtils.sendMessage(1003, Constants.ADAPT_HOME_FOLDER, new BandwidthMessage(10), true);
+                    System.out.println("Response: " + reply);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+
+
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
