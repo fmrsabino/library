254b2ce4e8f01a9e3a35aabbc49a86f8d53b5cc3
Daniel Porto <daniel.porto@gmail.com>
2016-05-09 14:16:55 +0100

    Simplifying the Bandwidth sensor for testing proposes.

--- src/bftsmart/demo/adapt/sensors/BandwidthSensor.java
+++ src/bftsmart/demo/adapt/sensors/BandwidthSensor.java
@@ -4,30 +4,13 @@ import bftsmart.demo.adapt.messages.sensor.BandwidthMessage;
 import bftsmart.demo.adapt.util.BftUtils;
 import bftsmart.demo.adapt.util.Constants;
 
+import java.util.Arrays;
+
 public class BandwidthSensor {
     public static void main(String[] args) {
+        System.out.println("Before sensor send the message to Adaptation Manager--");
+        byte[] reply = BftUtils.sendMessage(1003, Constants.ADAPT_HOME_FOLDER, new BandwidthMessage(10), true);
+        System.out.println("Response: " + Arrays.toString(reply));
 
-        new Thread(){
-            public void run(){
-                while(true){
-                    System.out.println("BandwidthSensor.run before sending message");
-                    byte[] reply = BftUtils.sendMessage(1003, Constants.ADAPT_HOME_FOLDER, new BandwidthMessage(10), true);
-                    System.out.println("Response: " + reply);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start();
-
-
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
