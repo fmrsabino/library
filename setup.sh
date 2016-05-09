5475450afed7d93a40c80b61774821e74aeda015
fmrsabino <fredx96@gmail.com>
2016-05-09 14:08:42 +0100

    Fixed config folder copying

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ mkdir -p -- "$workspaceDir"
 i=0
 while [ $i -lt "$nSensors" ]; do
 	mkdir -p -- "$workspaceDir/$sensorsDir$i"
-	cp -r "config/" "$workspaceDir/$sensorsDir$i"
+	cp -r "config" "$workspaceDir/$sensorsDir$i"
 	cp -r "adapt-config" "$workspaceDir/$sensorsDir$i"
 	cp -r "sensor" "$workspaceDir/$sensorsDir$i"
 	let i=i+1
@@ -19,7 +19,7 @@ echo "Created sensors workspace"
 i=0
 while [ $i -lt "$nAdapt" ]; do
 	mkdir -p -- "$workspaceDir/$adaptDir$i"
-	cp -r "config/" "$workspaceDir/$adaptDir$i"
+	cp -r "config" "$workspaceDir/$adaptDir$i"
 	cp -r "adapt-config" "$workspaceDir/$adaptDir$i"
 	let i=i+1
 done
@@ -27,7 +27,7 @@ echo "Created Adapt workspace"
 i=0
 while [ $i -lt "$nBft" ]; do
 	mkdir -p -- "$workspaceDir/$bftDir$i"
-	cp -r "config/" "$workspaceDir/$bftDir$i"
+	cp -r "config" "$workspaceDir/$bftDir$i"
 	let i=i+1
 done
 echo "Created BFT-SMaRt workspace"
